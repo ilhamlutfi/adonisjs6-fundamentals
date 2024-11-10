bf6b8ba789375e6245b4641ee8419aa210bb3739
ilhamlutfi <ilhamlutfi153@gmail.com>
2024-11-10 12:03:27 +0700

    2. Basics Routing

--- start/routes.ts
+++ start/routes.ts
@@ -11,6 +11,63 @@ import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 
-router.get('/user/:name', ({ params }) => {
-  return `Hello ${params.names}`
+// optional parameters
+router.get('/posts/:id?', ({ params }) => {
+  if (!params.id) {
+    return 'Showing all posts'
+  }
+
+  return `Showing post with id ${params.id}`
 })
+
+// Validate id to be numeric + cast to number data type
+router.where('id', router.matchers.number())
+
+// Validate id to be a valid UUID
+router.where('id', router.matchers.uuid())
+
+// Validate slug to match a given slug regex: regexr.com/64su0
+router.where('slug', router.matchers.slug())
+
+
+// group, prefix
+router
+  .group(() => {
+    router.get('users', ({ view }) => {
+      return view.render('users/index')
+    })
+    router.get('payments', () => {})
+  })
+  .prefix('/admin')
+
+// GET method, named route
+router.get('users', () => { }).as('user.index')
+
+// GET method, named route, controller
+router.get('users', [UserController, 'index']).as('user.index')
+
+// POST method
+router.post('users', () => {})
+
+// PUT method
+router.put('users/:id', () => {})
+
+// PATCH method
+router.patch('users/:id', () => {})
+
+// DELETE method
+router.delete('users/:id', () => {})
+
+// Redirect to a route
+router.on('/posts').redirect('/articles')
+
+// Redirect to a URL
+router.on('/posts').redirectToPath('https://youtube.com/@mubateknologi')
+
+// Resources
+ router.resource('/articles', () => {}).as('articles')
+
+
+
+
+
