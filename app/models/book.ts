be8a00769f3c408316dfe4c6828728aee0c086d3
ilhamlutfi <ilhamlutfi153@gmail.com>
2024-11-16 22:10:24 +0700

    5. basics model & migration

---
+++ app/models/book.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Book extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare slug: string
+
+  @column()
+  declare description: string
+
+  @column()
+  declare cover: string | null
+
+  @column.dateTime({
+    prepare: (value: DateTime) => value.toFormat('dd-MM-yyyy'),
+  })
+  declare publishedAt: DateTime
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
